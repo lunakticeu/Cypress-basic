27760a280eb8c7473879e41488a80b5df26b3126
Luna <lunakticeu@gmail.com>
2024-08-08 23:13:03 -0600

    add selectors

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,9 @@
 it('my first test', ()=> {
-  cy.visit('https://www.google.com/')
+  cy.visit('https://www.automationexercise.com/')
+  // selectors
+  cy.contains('cart')
+  cy.get('.fa-shopping-cart')
+    .first()
+  cy.get('.fa-shopping-cart')
+    .eq(0)
 })
